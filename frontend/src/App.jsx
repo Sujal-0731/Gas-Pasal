132ad2461d1b165f3f4c6d13276515c521b4e1bf
Sujal Shrestha <sujalst10@gmail.com>
2026-05-08 21:00:22 +0545

    minor

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -169,7 +169,7 @@ function App() {
               }}
               className="bg-red-500 hover:bg-red-600 text-white px-4 py-2 rounded-full text-sm font-semibold transition"
             >
-              🚪
+              🚪 
             </button>
           </div>
         </div>
